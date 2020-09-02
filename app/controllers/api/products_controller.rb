d416384c8300c28582cc7a95e5477a44a954ad3f
Jonathan Wang <jdaegonw@gmail.com>
2020-09-01 19:31:33 -0500

    added routes that display JSON for all 4 products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,17 @@ class Api::ProductsController < ApplicationController
     @product = Product.first 
     render "product_one.json.jb"
   end 
+  def product_two
+    @product = Product.second 
+    render "product_one.json.jb"
+  end 
+  def product_three
+    @product = Product.third 
+    render "product_one.json.jb"
+  end 
+  def product_four
+    @product = Product.last 
+    render "product_one.json.jb"
+  end 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/product_main" => "products#product_one"
+    get "/product_mainone" => "products#product_one"
+    get "/product_maintwo" => "products#product_two"
+    get "/product_mainthree" => "products#product_three"
+    get "/product_mainfour" => "products#product_four"
   end
 end
