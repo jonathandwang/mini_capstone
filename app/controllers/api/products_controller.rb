3f9ed01309cee22966578529de59b108b01fec7c
Jonathan Wang <jdaegonw@gmail.com>
2020-09-03 20:19:12 -0500

    used REST conventions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,23 +1,28 @@
 class Api::ProductsController < ApplicationController
-  def product_one
-    @product = Product.first 
-    render "product_one.json.jb"
-  end 
-  def product_two
-    @product = Product.second 
-    render "product_one.json.jb"
-  end 
-  def product_three
-    @product = Product.third 
-    render "product_one.json.jb"
-  end 
-  def product_four
-    @product = Product.last 
-    render "product_one.json.jb"
-  end 
-
-  def product_all
-    @all_product = Product.all
-    render "product_all.json.jb"
+  # def product_one
+  #   @product = Product.first 
+  #   render "product_one.json.jb"
+  # end 
+  # def product_two
+  #   @product = Product.second 
+  #   render "product_one.json.jb"
+  # end 
+  # def product_three
+  #   @product = Product.third 
+  #   render "product_one.json.jb"
+  # end 
+  # def product_four
+  #   @product = Product.last 
+  #   render "product_one.json.jb"
+  # end 
+  
+  def index
+    @product = Product.first
+    render "index.json.jb"
   end
+
+  def show
+    @product = Product.find_by(id:params[:id])
+    render "show.json.jb"
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/product_mainone" => "products#product_one"
-    get "/product_maintwo" => "products#product_two"
-    get "/product_mainthree" => "products#product_three"
-    get "/product_mainfour" => "products#product_four"
-    get "/product_all" => "products#product_all"
+    # get "/product_mainone" => "products#product_one"
+    # get "/product_maintwo" => "products#product_two"
+    # get "/product_mainthree" => "products#product_three"
+    # get "/product_mainfour" => "products#product_four"
+    get "/product" => "products#index"
+    get "/product/:id" => "products#show"
   end
 end
